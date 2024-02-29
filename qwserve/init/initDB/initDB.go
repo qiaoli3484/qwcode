3fcdb727c8137cd1eb01695b97853bd409e9c9ac
qiaoli3484 <574168030@qq.com>
2024-02-29 16:54:36 +0800

    33

--- qwserve/init/initDB/initDB.go
+++ qwserve/init/initDB/initDB.go
@@ -11,13 +11,16 @@ import (
 var dbg *gorm.DB
 
 func InitMysql() {
-	dsn := "root:@tcp(127.0.0.1:3306)/test_ims?charset=utf8mb4&loc=Local" //&parseTime=True
+	dsn := "root:@tcp(127.0.0.1:3306)/test?charset=utf8mb4&loc=Local&parseTime=True" //&parseTime=True
 	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 	dbg = d
 	sqlDB, err := d.DB()
+	if err != nil {
+		panic(err)
+	}
 	sqlDB.SetMaxIdleConns(10)
 	sqlDB.SetMaxOpenConns(500)
 	sqlDB.SetConnMaxLifetime(time.Hour)
--- qwserve/main.go
+++ qwserve/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"qwserve/init/initDB"
 	"qwserve/router"
 )
 
 func main() {
+	initDB.InitMysql() //数据库连接
+
 	r := router.InitRouter()
 
 	r.Run(":18967")
