5189863d4c754ab1e385a8ad6a9503da082e3c72
qiaoli3484 <574168030@qq.com>
2024-03-01 19:45:57 +0800

    3333

--- qwserve/models/models.go
+++ qwserve/models/models.go
@@ -4,6 +4,8 @@ import "gorm.io/gorm"
 
 type Product struct {
 	gorm.Model
-	Code  string `gorm:"type:varchar(65);default:'';comment:测试;"`
+	Name  string `gorm:"type:varchar(65);default:'';comment:名称;"`
+	Code  string `gorm:"type:varchar(65);default:'';comment:代码;"`
+	State uint8  `gorm:"type:tinyint;default:0;comment:状态;"`
 	Price uint
 }
