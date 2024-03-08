4e3529e0d8cc30ca6ae562cc0fa426c8bddafa11
qiaoli3484 <574168030@qq.com>
2024-03-08 21:19:46 +0800

    777

--- qwserve/router/router.go
+++ qwserve/router/router.go
@@ -8,12 +8,47 @@ import (
 
 func InitRouter() *gin.Engine {
 	r := gin.Default()
-	r.GET("/myComponent.vue", func(c *gin.Context) {
+	r.GET("/component/:name", func(c *gin.Context) {
 		res := app.Gin{c}
-		res.Response(200, "哈哈", "<template><h2>aaa</h2></template>")
+		name := c.Param("name")
+		s1 := ""
+		if name == "myComponent.vue" {
+			s1 = `
+			<template>
+			   <h2>{{a}}</h2>
+			   <qlcustom content="content.vue"/>
+			   <el-button>啊啊啊</el-button>
+			</template>
+			<script setup>
+			import {ref, watch,shallowRef, defineProps,defineAsyncComponent} from 'vue';
+			const a=ref("测试")
+			</script>
+			`
+		} else if name == "content.vue" {
+			s1 = `
+			<template>
+			   <h2>{{a}}</h2>
+			   <el-button>啊啊啊</el-button>
+			</template>
+			<script setup>
+			import {ref, watch,shallowRef, defineProps,defineAsyncComponent} from 'vue';
+			const a=ref("测试22")
+			</script>
+			`
+		}
+
+		res.Response(200, "哈哈", s1)
 		//c.String(200, "<template><h2>aaa</h2></template>")
 	})
 
 	//<template><h2>aaa</h2></template>
 	return r
+}
+
+//字段->转换成元素
+
+type 元素 struct {
+	component string //组件名称 版本?
+	data      string
+	childrens []*元素
 }
